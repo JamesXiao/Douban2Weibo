302ec782afdb17dab9a235117d258dc6d42b969a
JamesXiao <xiaoyang0304@gmail.com>
2011-06-10 20:47:52 +0800

    main logic and link to sina

---
+++ douban2weibo.js
@@ -0,0 +1,59 @@
+// ==UserScript==
+// @name         Share Douban Item to Sina Weibo
+// @namespace    http://xydonkey.blogbus.com/
+// @include       http://movie.douban.com/subject/*
+// @include       http://music.douban.com/subject/*
+// @include       http://book.douban.com/subject/*
+// @description  分享豆瓣条目至新浪微博
+// under GPL 3.0 Lisence.
+// ==/UserScript==
+
+
+
+//题目
+function getTitle(){
+	return "书名";
+}
+
+//评分：力荐、推荐、还行、较差、很差、默认值是空字符串
+function getRating(){
+    return "，力荐，";
+}
+
+//短评
+function getComment(){
+    return "这本书不错";
+}
+
+//状态，想读、在读、读过、默认值是"推荐"
+function getState(){
+    return "推荐";
+}
+
+//组装微博内容
+function generateWeiBo(){
+    return "我"+getState()+"《"+getTitle()+"》" +getRating()+ getComment();
+}
+
+//封面地址
+function getCover(){
+    return '';
+}
+
+
+var _w = 72 , _h = 16;
+var param = {
+	url:	location.href,
+	type:	'3',
+	title:	generateWeiBo(), 
+	pic:   	getCover(), 
+	rnd:	new Date().valueOf()
+}
+var temp = [];
+for( var p in param ){
+    temp.push(p + '=' + encodeURIComponent( param[p] || '' ) )
+}
+var share2Weibo = document.createElement('div');
+share2Weibo.innerHTML += '<iframe allowTransparency="true" frameborder="0" scrolling="no" src="http://hits.sinajs.cn/A1/weiboshare.html?' + temp.join('&') + '" width="'+ _w+'" height="'+_h+'"></iframe>';
+var rating = document.getElementById('rating');
+var htmlContent =  rating.appendChild(share2Weibo);
